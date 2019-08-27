1d0acc9df09b1f2fefbcaf00667301b454422600
dougtq <douglas.expalves@gmail.com>
2019-08-26 23:58:29 -0300

    folder and libs configuration

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "username": "docker",
+  "password": "docker",
+  "database": "auth",
+  "host": "127.0.0.1",
+  "dialect": "postgres",
+  "operatorsAliases": false,
+  "logging": false,
+  "define": {
+    "timestamps": true,
+    "underscored": true,
+    "underscoredAll": true
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const Router = require("express").Router()
+
+// importação de rotas ao projeto
+
+
+module.exports = Router
